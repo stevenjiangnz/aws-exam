444a2e7e7a124f7a3c9c194101748bab8d7295be
Steven Jiang <stevenjaingnz@gmail.com>
2020-07-04 19:34:32 +1000

    add default 8080

--- study-sync/index.js
+++ study-sync/index.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const port = process.env.PORT
-
-console.log("process.env.PORT", process.env.PORT)
+const port = process.env.PORT || 8080
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
